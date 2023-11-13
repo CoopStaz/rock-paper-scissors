4833462c3ffe309d42433e865bb8474784cc959c
Cooper Stasinowsky <cjstaz13@gmail.com>
2023-11-13 16:31:50 +1100

    Begin creating a function that plays a single round

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 //Declare getComputerChoice function
 //Create array with possible choices
 //Create variable that randomizes the array
+let computerChoice;
+
 function getComputerChoice() {
     const gameOptions = ["rock", "paper", "scissors"];
-    let computerChoice = Math.ceil(Math.random() * gameOptions.length - 1);
+    computerChoice = Math.ceil(Math.random() * gameOptions.length - 1);
 
     if (computerChoice == 1) {
         return (computerChoice = "rock");
@@ -13,3 +15,21 @@ function getComputerChoice() {
         return (computerChoice = "scissors");
     }
 }
+
+//Create a function
+//Function must take two parameters: playerSelection and computerSelection
+//Use prompt for user input
+let playerSelection = prompt("What do you choose?");
+
+let computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    
+    if (playerSelection == computerSelection) {
+        return `Draw! ${playerSelection} and ${computerSelection} are the same.`;
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        return "You lose! Paper beats rock.";
+    }
+}
+
+console.log(playerSelection);
