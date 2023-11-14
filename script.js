d84c75b652c8b29c778da7166d57a82d8568f22f
Cooper Stasinowsky <cjstaz13@gmail.com>
2023-11-14 16:04:02 +1100

    Add playRound() function that plays a single round of rock, paper, scissors.

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 //Declare getComputerChoice function
 //Create array with possible choices
 //Create variable that randomizes the array
-let computerChoice;
 
 function getComputerChoice() {
     const gameOptions = ["rock", "paper", "scissors"];
-    computerChoice = Math.ceil(Math.random() * gameOptions.length - 1);
+    let computerChoice = Math.ceil(Math.random() * gameOptions.length - 1);
 
     if (computerChoice == 1) {
         return (computerChoice = "rock");
@@ -19,17 +18,28 @@ function getComputerChoice() {
 //Create a function
 //Function must take two parameters: playerSelection and computerSelection
 //Use prompt for user input
-let playerSelection = prompt("What do you choose?");
-
-let computerSelection = getComputerChoice();
 
 function playRound(playerSelection, computerSelection) {
+    playerSelection = prompt("What do you choose?").toLowerCase();
+    computerSelection = getComputerChoice();
     
     if (playerSelection == computerSelection) {
         return `Draw! ${playerSelection} and ${computerSelection} are the same.`;
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "You win! Rock beats scissors.";
     } else if (playerSelection == "rock" && computerSelection == "paper") {
         return "You lose! Paper beats rock.";
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return "You lose! Scissors beats paper.";
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        return "You win! Paper beats rock.";
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "You win! Scissors beats paper.";
+    } else {
+        return "You lose! Rock beats scissors";
     }
+
+    console.log(playerSelection);
+    console.log(computerSelection);
 }
 
-console.log(playerSelection);
